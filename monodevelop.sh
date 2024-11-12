e892625de4c1db24d3772bcad626c80dccf221d5
Daniel Prince <126276625+danielprinceD@users.noreply.github.com>
2024-11-12 21:37:00 +0530

    Update monodevelop.sh

--- monodevelop.sh
+++ monodevelop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yes | sudo apt install gnupg ca-certificates
+yes | sudo apt install gnupg 
+yes | sudo apt install ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys
 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo
