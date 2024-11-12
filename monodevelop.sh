e9f124ffdd38e6a4800ba872193f9f02ab1d0d1a
Daniel Prince <126276625+danielprinceD@users.noreply.github.com>
2024-11-12 21:39:37 +0530

    Update monodevelop.sh

--- monodevelop.sh
+++ monodevelop.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 yes | sudo apt install gnupg 
 yes | sudo apt install ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys
-3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 yes | sudo apt-get install monodevelop
 echo "\n\n\n\n Adutha Lab kaachum padichitu vaa da venna"
