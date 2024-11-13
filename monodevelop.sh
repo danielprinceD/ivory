718196c400f2938606b8176f2cc21876317f6073
Daniel Prince <126276625+danielprinceD@users.noreply.github.com>
2024-11-13 05:36:18 +0530

    Update monodevelop.sh

--- monodevelop.sh
+++ monodevelop.sh
@@ -4,4 +4,8 @@ yes | sudo apt install ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 yes | sudo apt-get install monodevelop
+yes | sudo apt-get install mono-devel
+yes | sudo apt-get install mono-complete
+yes | sudo apt-get install mono-dbg
+yes | sudo apt-get install ca-certificates-mono
 echo -e "\n\n\n\n\n\nDei Parama Padida \n\n"
